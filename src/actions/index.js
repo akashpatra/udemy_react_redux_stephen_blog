6da34042ed1d5c39b0ec7f15c3321ea3fbf01308
Akash Patra <patraakash9@gmail.com>
2020-06-11 16:30:14 +0530

    Organized and commented different way to write ActionCreators.

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,6 +12,18 @@ export const fetchPosts = () => async dispatch => {
 };
 
 // Function which returns a Function
+// export const fetchUser = function(id) { 
+//     return async function(dispatch) {
+//         const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//         dispatch({
+//             type: 'FETCH_USER',
+//             payload: response.data
+//         })
+//     }
+// };
+
+// Above one using Arrow Functions
 // export const fetchUser = (id) => async dispatch => {
 //     const response = await jsonPlaceholder.get(`/users/${id}`);
 
@@ -21,26 +33,24 @@ export const fetchPosts = () => async dispatch => {
 //     })
 // };
 
-// Switching Arrow function to below Normal keyword function
-// export const fetchUser = function(id) { 
-//     return _.memoize(async function(dispatch) {
-//         const response = await jsonPlaceholder.get(`/users/${id}`);
+// Above one with _.memoize
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
 
-//         dispatch({
-//             type: 'FETCH_USER',
-//             payload: response.data
-//         })
+//     dispatch({
+//         type: 'FETCH_USER',
+//         payload: response.data
 //     })
-// };
+// });
 
-export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+// Alternate Overfetching
+export const fetchUser = (id) => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
 
     dispatch({
         type: 'FETCH_USER',
         payload: response.data
     })
-});
+};
 

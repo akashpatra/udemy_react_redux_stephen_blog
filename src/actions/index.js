dff6e44bc39443d926cbd67c4c3fced2d4d5cf19
Akash Patra <patraakash9@gmail.com>
2020-06-11 14:30:04 +0530

    One Time Memoization

--- src/actions/index.js
+++ src/actions/index.js
@@ -22,13 +22,25 @@ export const fetchPosts = () => async dispatch => {
 // };
 
 // Switching Arrow function to below Normal keyword function
-export const fetchUser = function(id) { 
-    return _.memoize(async function(dispatch) {
-        const response = await jsonPlaceholder.get(`/users/${id}`);
+// export const fetchUser = function(id) { 
+//     return _.memoize(async function(dispatch) {
+//         const response = await jsonPlaceholder.get(`/users/${id}`);
 
-        dispatch({
-            type: 'FETCH_USER',
-            payload: response.data
-        })
+//         dispatch({
+//             type: 'FETCH_USER',
+//             payload: response.data
+//         })
+//     })
+// };
+
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({
+        type: 'FETCH_USER',
+        payload: response.data
     })
-};
+});
+
